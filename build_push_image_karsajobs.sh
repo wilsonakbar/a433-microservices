1f1e05d7213d236a2e315a3bc6c0b42dd836d063
WE <we@MacBook-Pro-MBPro.local>
2025-07-28 22:26:51 +0700

    update

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-set -e
 
-# Tag image sesuai username kamu
-docker tag karsajobs:latest docker.io/wilsonakbar/karsajobs:latest
+docker build -t wilsonakbar/karsajobs:latest .
+docker login
+docker push wilsonakbar/karsajobs:latest
 
-# Login dan push
-echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
-docker push docker.io/wilsonakbar/karsajobs:latest
+
+
+
+
+
+
+
+
+
+
 
